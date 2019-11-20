bb41f36dd850759120d6a9bc4347959bd4bb576b
Ádám Gulyás <gulyo2000@gmail.com>
2019-11-20 07:58:49 +0100

    deleting demo files

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,4 @@
 import "./app.imports";
-import STYLE from "./index.module.scss";
-import "./index.scss";
 import { logBouncyBob } from "./util/logConfig";
 
 logBouncyBob.info({ msg: "Hello World" });
-$("[property='test']").addClass(STYLE.test);
