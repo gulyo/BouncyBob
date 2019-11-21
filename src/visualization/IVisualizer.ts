cfe7bf94eeab377993d9fe1dd35261f646c0ff7e
Gulyás Ádám <adam.gulyas@visoft.hu>
2019-11-21 18:38:01 +0100

    Basic structure of interfaces is ready

---
+++ src/visualization/IVisualizer.ts
@@ -0,0 +1,7 @@
+import IProduct from "../base/IProduct";
+import IProductConfig from "../base/IProductConfig";
+
+export default interface IVisualizer<TConfig extends IProductConfig> extends IProduct<TConfig> {
+  Show(): void;
+  Hide(): void;
+}
---
+++ src/visualization/world/IWorld.ts
@@ -0,0 +1,4 @@
+import IVisualizer from "../IVisualizer";
+import IWorldConfig from "./IWorldConfig";
+
+export default interface IWorld extends IVisualizer<IWorldConfig> {}
